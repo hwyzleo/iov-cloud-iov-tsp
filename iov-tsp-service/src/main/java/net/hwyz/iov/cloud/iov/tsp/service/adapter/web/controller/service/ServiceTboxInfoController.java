89a69a73c052c05b8051dbd5e3f1441fb1f71b17
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-05-18 20:52:32 +0800

    重构: 重构TBOX相关业务

--- iov-tsp-service/src/main/java/net/hwyz/iov/cloud/iov/tsp/service/adapter/web/controller/service/ServiceTboxInfoController.java
+++ iov-tsp-service/src/main/java/net/hwyz/iov/cloud/iov/tsp/service/adapter/web/controller/service/ServiceTboxInfoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/service/tbox")
+@RequestMapping(value = "/api/service/tbox/v1")
 public class ServiceTboxInfoController {
 
     private final TboxInfoAppService tboxInfoAppService;
