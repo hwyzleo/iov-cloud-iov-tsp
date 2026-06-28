9a567abd61a88e218b5b6f8f1147da3e6ffe2f74
hwyz_leo <hwyz.leo.ye@gmail.com>
2026-06-28 10:03:26 +0800

    fix: complete Assembler mapping for checkDetails

--- iov-tsp-service/src/main/java/net/hwyz/iov/cloud/iov/tsp/service/application/assembler/DeviceAdmissionAssembler.java
+++ iov-tsp-service/src/main/java/net/hwyz/iov/cloud/iov/tsp/service/application/assembler/DeviceAdmissionAssembler.java
@@ -4,6 +4,7 @@ import net.hwyz.iov.cloud.iov.tsp.service.application.dto.result.DeviceAdmission
 import net.hwyz.iov.cloud.iov.tsp.service.domain.deviceAdmission.model.DeviceAdmission;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 /**
@@ -20,6 +21,30 @@ public interface DeviceAdmissionAssembler {
     @Mapping(source = "deviceStatusCheckResult.status.code", target = "deviceStatus")
     @Mapping(source = "createTime", target = "updatedAt")
     @Mapping(target = "bindVersion", ignore = true)
-    @Mapping(target = "checkDetails", ignore = true)
+    @Mapping(source = ".", target = "checkDetails", qualifiedByName = "toCheckDetails")
     DeviceAdmissionCheckResult toResult(DeviceAdmission deviceAdmission);
+
+    @Named("toCheckDetails")
+    default DeviceAdmissionCheckResult.CheckDetails toCheckDetails(DeviceAdmission deviceAdmission) {
+        if (deviceAdmission == null) {
+            return null;
+        }
+
+        return DeviceAdmissionCheckResult.CheckDetails.builder()
+            .hsmCheck(toCheckResult(deviceAdmission.getHsmCheckResult()))
+            .pkiCheck(toCheckResult(deviceAdmission.getPkiCheckResult()))
+            .deviceStatusCheck(toCheckResult(deviceAdmission.getDeviceStatusCheckResult()))
+            .build();
+    }
+
+    default DeviceAdmissionCheckResult.CheckResult toCheckResult(DeviceAdmission.CheckResult checkResult) {
+        if (checkResult == null) {
+            return null;
+        }
+
+        return DeviceAdmissionCheckResult.CheckResult.builder()
+            .status(checkResult.getStatus().getCode())
+            .message(checkResult.getMessage())
+            .build();
+    }
 }
